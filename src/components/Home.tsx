8a6df27355c8748a39735f1a1b8fc411b2108d03
jasper-ang <jasper.zy.ang@gmail.com>
2024-07-24 12:23:01 +0100

    updated home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,12 +6,15 @@ const Home: React.FC = () => {
   return (
     <div id="home">
       <h1>Jasper's Portfolio</h1>
+      <br></br>
       <hr></hr>
+      <br></br>
       <img src={jasperImage} alt="Jasper" height = "200" className="example-image" />
       <br></br>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio repellat sapiente provident, voluptatem temporibus illo quod, voluptas reprehenderit aliquam enim nisi eaque laudantium doloremque esse! Libero aperiam repellendus ipsa error.</p>
       <br></br>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi nemo provident est ipsam, non impedit delectus voluptatem soluta, maxime ad obcaecati minus autem temporibus veritatis facere! Saepe aliquam quasi nihil?</p>
+      <br></br>
     </div>
   );
 };
