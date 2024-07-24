a8a6c035f776c32c4c9470966075e9f647febcbb
jasper-ang <jasper.zy.ang@gmail.com>
2024-07-24 02:08:51 +0100

    updated picture

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Home.css';
+import jasperImage from '../assets/jasper.jpg';
 
 const Home: React.FC = () => {
   return (
     <div id="home">
       <h1>Welcome to My Portfolio</h1>
       <p>This is the home section.</p>
+      <img src={jasperImage} alt="Jasper" className="example-image" />
     </div>
   );
 };
