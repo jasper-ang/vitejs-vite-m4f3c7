5973a70cba98a10909877829704167601bb27530
jasper-ang <jasper.zy.ang@gmail.com>
2024-07-25 14:46:44 +0100

    reboot

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Home from './components/Home'
 import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
-import './App.css' // Import global styles
+import './styles/App.css' // Correct path to App.css
 
 const App: React.FC = () => {
   return (
